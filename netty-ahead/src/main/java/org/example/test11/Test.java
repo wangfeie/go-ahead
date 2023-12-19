7ad9b3f3553f42dc35f7a2889ff76e1b78e9f3b0
wangfeie <wangfeie@yonyou.com>
2023-12-19 20:37:40 +0800

    add test

--- netty-ahead/src/main/java/org/example/test11/Test.java
+++ netty-ahead/src/main/java/org/example/test11/Test.java
@@ -1,10 +1,16 @@
 package org.example.test11;
 
 /**
+ * 新的测试
+ *
  * @author wangfeie
  * @version 1.0.0
- * @description #TODO
  * @date 2023/12/18 19:30
  */
 public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
 }
