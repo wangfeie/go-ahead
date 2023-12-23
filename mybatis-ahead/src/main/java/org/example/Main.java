ca23c22e5ee1aaa47c614f3fdd83511ce399f891
wangfeie <wangfeie@yonyou.com>
2023-12-23 21:36:11 +0800

    add mybatis test

--- mybatis-ahead/src/main/java/org/example/Main.java
+++ mybatis-ahead/src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        InputStream xml = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(xml);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
     }
 }
