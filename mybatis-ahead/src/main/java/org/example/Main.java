6a32625343e88c23b4004cfe4838b7a0b8e92137
wangfeie <wangfeie@yonyou.com>
2023-12-24 22:16:26 +0800

    mybatis test change

--- mybatis-ahead/src/main/java/org/example/Main.java
+++ mybatis-ahead/src/main/java/org/example/Main.java
@@ -7,11 +7,16 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
         InputStream xml = Resources.getResourceAsStream("mybatis-config.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(xml);
         SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        Connection connection = sqlSession.getConnection();
+        connection.close();
     }
 }
