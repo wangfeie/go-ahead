b58b68fcc81fafb6530151e6411d2d248157f907
wangfeie <wangfeie@yonyou.com>
2024-01-05 09:41:07 +0800

    mybatis test a little change

--- mybatis-ahead/src/main/java/org/example/Main.java
+++ mybatis-ahead/src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.mapping.ResultMap;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -9,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) throws IOException, SQLException {
@@ -17,6 +20,8 @@ public class Main {
         SqlSession sqlSession = sqlSessionFactory.openSession();
 
         sqlSession.clearCache();
+        Configuration configuration = sqlSession.getConfiguration();
+        Collection<ResultMap> resultMaps = configuration.getResultMaps();
 
         Connection connection = sqlSession.getConnection();
         connection.close();
